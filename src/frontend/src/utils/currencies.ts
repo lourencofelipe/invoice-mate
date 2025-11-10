767622db187ecf7e1bf93871a79f73ad46c04aac
lourencofelipe <felipe.pucsc@gmail.com>
2025-11-10 15:04:40 +1300

    refac: Adding currency notation for each type.

--- src/frontend/src/utils/currencies.ts
+++ src/frontend/src/utils/currencies.ts
@@ -1,10 +1,10 @@
 export const currencies = [
-    { value: "NZD", labelPt: "Dólar neozelandês", labelEn: "New Zealand dollar" },
-    { value: "USD", labelPt: "Dólar americano", labelEn: "American dollar" },
-    { value: "EUR", labelPt: "Euro", labelEn: "Euro" },
-    { value: "GBP", labelPt: "Libra esterlina", labelEn: "English pound" },
-    { value: "AUD", labelPt: "Dólar australiano", labelEn: "Australian dollar" },
-    { value: "CAD", labelPt: "Dólar canadense", labelEn: "Canadian dollar" },
-    { value: "CHF", labelPt: "Franco suíço", labelEn: "Swiss franc" },
-    { value: "JPY", labelPt: "Iene japonês", labelEn: "Japanese yen" },
+  { value: "NZD", labelPt: "Dólar neozelandês", labelEn: "NZD - New Zealand dollar" },
+  { value: "AUD", labelPt: "Dólar australiano", labelEn: "AUD - Australian dollar" },
+  { value: "USD", labelPt: "Dólar americano", labelEn: "USD - American dollar" },
+  { value: "EUR", labelPt: "Euro", labelEn: "EUR - Euro" },
+  { value: "GBP", labelPt: "Libra esterlina", labelEn: "GBP - British pound" },
+  { value: "CAD", labelPt: "Dólar canadense", labelEn: "CAD - Canadian dollar" },
+  { value: "CHF", labelPt: "Franco suíço", labelEn: "CHF - Swiss franc" },
+  { value: "JPY", labelPt: "Iene japonês", labelEn: "JPY - Japanese yen" },  
   ];
