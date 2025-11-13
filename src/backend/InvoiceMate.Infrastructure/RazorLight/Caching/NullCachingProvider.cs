b7c5f1073264d95376641bd22b26c9513d9f25aa
lourencofelipe <felipe.pucsc@gmail.com>
2025-11-13 13:27:21 +1300

    feat: PH3-05-Phase 3 - Added a temporary NullCaching class to avoid razor getting the template from cache during development.

---
+++ src/backend/InvoiceMate.Infrastructure/RazorLight/Caching/NullCachingProvider.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Primitives;
+using RazorLight.Caching;
+
+public class NullCachingProvider : ICachingProvider
+{
+    /// <summary>
+    /// Igonres cache.
+    /// </summary>
+    public void CacheTemplate(string key, Func<ITemplatePage> pageFactory, IChangeToken expirationToken)
+    {
+      
+    }
+
+    /// <summary>
+    /// It always returns false to force recompilation.
+    /// </summary>
+    public bool Contains(string key)
+    {
+        return false;
+    }
+
+    public void Remove(string key)
+    {
+
+    }
+
+    /// <summary>
+    /// It always returns "not found", forcing RazorLight to recompile the template.
+    /// </summary>
+    public TemplateCacheLookupResult RetrieveTemplate(string key)
+    {
+        return new TemplateCacheLookupResult();
+    }
+}
